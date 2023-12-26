94c67f4cf6b23e64c1af471d44b2f8bb57d82101
S-Sarim <134587091+S-Sarim@users.noreply.github.com>
2023-12-26 18:19:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
 import inquirer from 'inquirer';
 import qrcode from 'qrcode';
-import fs from "fs"
+import readline from 'readline';
+import fs from 'fs';
+import { url } from 'inspector';
+import path from 'path';
 
-inquirer
-  .prompt([
-    {
-      message: "Enter the website URL: ",
-      name: "url",
-    },
-  ])
-  .then((answers) => {
-    const url = answers.url;
-    const fileName = url.replace(/\.com/g, '')
+const filePath = './getUrls.txt';
+const urlsToGenerate = [];
+const folder = 'Generated Images';
 
-    qrcode.toFile(fileName + '.png', url, (err) => {
-      if (err) {
-        console.error('Error generating QR code:', err);
-      } else {
-        console.log('QR code generated successfully! Check ' + fileName + '.png');
-      }
-    });
+if (!fs.existsSync(folder)) {
+  fs.mkdirSync(folder);
+}
 
+
+const readStream = fs.createReadStream(filePath);
+const rl = readline.createInterface({
+  input: readStream,
+  crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+  urlsToGenerate.push(line);
+});
+
+rl.on('close', () => {
+  for (const line of urlsToGenerate) {
+    const url = line;
+    const fileName = url.replace(/\.com/g, '');
+    const filePath = path.join(folder,  fileName + '.png')
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+    qrcode.toFile(filePath, url, (err) => {
+            if (err) {
+              console.error('Error generating QR code:', err);
+            } else {
+              console.log('QR code generated successfully! Check ' + filePath);
+            }
+          });
     fs.appendFile("./URL.txt", "\n" +  url, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      }); 
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+      if (err) throw err;
+      console.log('The file has been saved!');
+    });
+  }
+});
